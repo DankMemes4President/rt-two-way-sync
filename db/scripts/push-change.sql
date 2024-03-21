2559dcaf83e372d1ab121213b92b88a6a27467e9
Harsh Singh <harshsingh050803@gmail.com>
2024-03-21 21:50:16 +0530

    feat: add json formatting to payload

--- db/scripts/push-change.sql
+++ db/scripts/push-change.sql
@@ -1,7 +1,10 @@
 -- create function
 CREATE or REPLACE FUNCTION push_change() RETURNS TRIGGER AS $$
+    DECLARE
+        payload json;
     BEGIN
-        PERFORM pg_notify('changes', tg_table_name || ',' || tg_op || ',' || new.id);
+        payload := row_to_json(new);
+        PERFORM pg_notify('changes', json_build_array(tg_table_name, tg_op, payload)::text);
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
