49f578edd9d778ea858ff9cfe1f8e72eabeac436
Harsh Singh <harshsingh050803@gmail.com>
2024-03-21 17:29:58 +0530

    feat: add db trigger to function creation script

--- db/scripts/push-change.sql
+++ db/scripts/push-change.sql
@@ -1,6 +1,11 @@
+-- create function
 CREATE or REPLACE FUNCTION push_change() RETURNS TRIGGER AS $$
     BEGIN
         PERFORM pg_notify('changes', tg_table_name || ',' || tg_op || ',' || new.id);
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
+
+-- create trigger
+CREATE TRIGGER customer_change AFTER INSERT OR UPDATE OR DELETE ON customers
+    FOR EACH ROW EXECUTE FUNCTION push_change();
